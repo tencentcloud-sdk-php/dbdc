0396d710bbdca024912c83d01160f1953842bed6
tencentcloudapi <tencentcloudapi@tencent.com>
2026-09-07 04:21:09 +0800

    release 3.0.1663

--- src/TencentCloud/Dbdc/V20201029/Models/ModifyDBCustomDisasterRecoverGroupTagsRequest.php
+++ src/TencentCloud/Dbdc/V20201029/Models/ModifyDBCustomDisasterRecoverGroupTagsRequest.php
@@ -18,24 +18,24 @@ namespace TencentCloud\Dbdc\V20201029\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * ModifyDBCustomNodeTags请求参数结构体
+ * ModifyDBCustomDisasterRecoverGroupTags请求参数结构体
  *
- * @method string getNodeId() 获取<p>DB Custom 节点ID</p><p>参数格式：dbcn-0zan5xxk</p>
- * @method void setNodeId(string $NodeId) 设置<p>DB Custom 节点ID</p><p>参数格式：dbcn-0zan5xxk</p>
- * @method array getAddTags() 获取<p>为节点绑定的标签信息</p><p>入参限制：参考标签侧的限制</p><p>如果节点未关联输入的标签键，则增加关联；若已关联，则将该节点关联的键对应的标签值修改为输入值。本接口中 AddTags 和 DeleteTagKeys 二者必须存在其一，且二者不能包含相同的标签键。</p>
- * @method void setAddTags(array $AddTags) 设置<p>为节点绑定的标签信息</p><p>入参限制：参考标签侧的限制</p><p>如果节点未关联输入的标签键，则增加关联；若已关联，则将该节点关联的键对应的标签值修改为输入值。本接口中 AddTags 和 DeleteTagKeys 二者必须存在其一，且二者不能包含相同的标签键。</p>
+ * @method string getDisasterRecoverGroupId() 获取<p>置放群组ID</p>
+ * @method void setDisasterRecoverGroupId(string $DisasterRecoverGroupId) 设置<p>置放群组ID</p>
+ * @method array getAddTags() 获取<p>为 DB Custom 置放群组绑定的标签信息</p><p>入参限制：参考标签侧的限制</p><p>如果置放群组未关联输入的标签键，则增加关联；若已关联，则将该置放群组关联的键对应的标签值修改为输入值。本接口中 AddTags 和 DeleteTagKeys 二者必须存在其一，且二者不能包含相同的标签键。</p>
+ * @method void setAddTags(array $AddTags) 设置<p>为 DB Custom 置放群组绑定的标签信息</p><p>入参限制：参考标签侧的限制</p><p>如果置放群组未关联输入的标签键，则增加关联；若已关联，则将该置放群组关联的键对应的标签值修改为输入值。本接口中 AddTags 和 DeleteTagKeys 二者必须存在其一，且二者不能包含相同的标签键。</p>
  * @method array getDeleteTagKeys() 获取<p>需要解关联的标签Key</p><p>本接口中 AddTags 和 DeleteTagKeys 二者必须存在其一，且二者不能包含相同的标签键。</p>
  * @method void setDeleteTagKeys(array $DeleteTagKeys) 设置<p>需要解关联的标签Key</p><p>本接口中 AddTags 和 DeleteTagKeys 二者必须存在其一，且二者不能包含相同的标签键。</p>
  */
-class ModifyDBCustomNodeTagsRequest extends AbstractModel
+class ModifyDBCustomDisasterRecoverGroupTagsRequest extends AbstractModel
 {
     /**
-     * @var string <p>DB Custom 节点ID</p><p>参数格式：dbcn-0zan5xxk</p>
+     * @var string <p>置放群组ID</p>
      */
-    public $NodeId;
+    public $DisasterRecoverGroupId;
 
     /**
-     * @var array <p>为节点绑定的标签信息</p><p>入参限制：参考标签侧的限制</p><p>如果节点未关联输入的标签键，则增加关联；若已关联，则将该节点关联的键对应的标签值修改为输入值。本接口中 AddTags 和 DeleteTagKeys 二者必须存在其一，且二者不能包含相同的标签键。</p>
+     * @var array <p>为 DB Custom 置放群组绑定的标签信息</p><p>入参限制：参考标签侧的限制</p><p>如果置放群组未关联输入的标签键，则增加关联；若已关联，则将该置放群组关联的键对应的标签值修改为输入值。本接口中 AddTags 和 DeleteTagKeys 二者必须存在其一，且二者不能包含相同的标签键。</p>
      */
     public $AddTags;
 
@@ -45,8 +45,8 @@ class ModifyDBCustomNodeTagsRequest extends AbstractModel
     public $DeleteTagKeys;
 
     /**
-     * @param string $NodeId <p>DB Custom 节点ID</p><p>参数格式：dbcn-0zan5xxk</p>
-     * @param array $AddTags <p>为节点绑定的标签信息</p><p>入参限制：参考标签侧的限制</p><p>如果节点未关联输入的标签键，则增加关联；若已关联，则将该节点关联的键对应的标签值修改为输入值。本接口中 AddTags 和 DeleteTagKeys 二者必须存在其一，且二者不能包含相同的标签键。</p>
+     * @param string $DisasterRecoverGroupId <p>置放群组ID</p>
+     * @param array $AddTags <p>为 DB Custom 置放群组绑定的标签信息</p><p>入参限制：参考标签侧的限制</p><p>如果置放群组未关联输入的标签键，则增加关联；若已关联，则将该置放群组关联的键对应的标签值修改为输入值。本接口中 AddTags 和 DeleteTagKeys 二者必须存在其一，且二者不能包含相同的标签键。</p>
      * @param array $DeleteTagKeys <p>需要解关联的标签Key</p><p>本接口中 AddTags 和 DeleteTagKeys 二者必须存在其一，且二者不能包含相同的标签键。</p>
      */
     function __construct()
@@ -62,8 +62,8 @@ class ModifyDBCustomNodeTagsRequest extends AbstractModel
         if ($param === null) {
             return;
         }
-        if (array_key_exists("NodeId",$param) and $param["NodeId"] !== null) {
-            $this->NodeId = $param["NodeId"];
+        if (array_key_exists("DisasterRecoverGroupId",$param) and $param["DisasterRecoverGroupId"] !== null) {
+            $this->DisasterRecoverGroupId = $param["DisasterRecoverGroupId"];
         }
 
         if (array_key_exists("AddTags",$param) and $param["AddTags"] !== null) {
